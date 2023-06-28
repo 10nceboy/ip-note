84a36c98310429a364e2436f2f84eb5db818ce9f
10nce <10nceboy@gmail.com>
2023-06-29 00:41:09 +0400

    fix fade animation for header

--- src/js/effects/fade.js
+++ src/js/effects/fade.js
@@ -3,11 +3,8 @@ let deviceType = getDeviceType();
 const cards = document.querySelectorAll('.flip-card');
 
 if (['tablet', 'laptop'].includes(deviceType)) {
-  [
-    document.querySelector('.menu'),
-    document.querySelector('.main-header')
-  ].forEach((el) => el.classList.add('fade-in'));
-
+  document.querySelector('.menu').classList.add('fade-in-menu');
+  document.querySelector('.main-header').classList.add('fade-in-header');
   document.querySelector('.reveal').classList.add('reveal-animated');
 
   cards.forEach((card) => {
