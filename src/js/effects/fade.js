8dac5b9192cf00a3054ba8e746a718d4470c2de1
10nce <10nceboy@gmail.com>
2023-06-28 16:40:26 +0400

    fix blur, add left-right fade

--- src/js/effects/fade.js
+++ src/js/effects/fade.js
@@ -4,10 +4,9 @@ let deviceType = getDeviceType();
 const cards = document.querySelectorAll('.flip-card');
 
 if (['tablet', 'laptop'].includes(deviceType)) {
-  [
-    document.querySelector('.main-header'),
-    document.querySelector('.menu')
-  ].forEach((el) => el.classList.add('fade-in'));
+  [document.querySelector('.menu')].forEach((el) =>
+    el.classList.add('fade-in')
+  );
 
   cards.forEach((card) => {
     let cardIndex = card.dataset.card;
